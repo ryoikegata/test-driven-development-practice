59233a2c9498086b2edb2e2ac937a8a4130a99a2
Sakinom <sakinom328@gmail.com>
2024-08-16 15:30:36 +0900

    最終形態

--- src/Dollar.php
+++ src/Dollar.php
@@ -5,8 +5,12 @@ class Dollar
   public int $amount;
 
   public function __construct(int $amount)
-  {}
+  {
+    $this->amount = $amount;
+  }
 
   public function times(int $multiplier): void
-  {}
+  {
+    $this->amount *= $multiplier;
+  }
 }
