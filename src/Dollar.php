ba37d0f92feede09a115be2dc6f80eed91b8cc1e
Sakinom <sakinom328@gmail.com>
2024-09-06 09:49:14 +0900

    chapter5終了

--- src/Dollar.php
+++ src/Dollar.php
@@ -20,3 +20,24 @@ class Dollar
     return $this->amount === $dollar->amount;
   }
 }
+
+class Franc
+{
+  private int $amount;
+
+  public function __construct(int $amount)
+  {
+    $this->amount = $amount;
+  }
+
+  public function times(int $multiplier): Franc
+  {
+    return new Franc($this->amount * $multiplier);
+  }
+
+  public function equals(object $object)
+  {
+    $dollar = $object;
+    return $this->amount === $dollar->amount;
+  }
+}
