f6614ff3f8542230400b28708f09498359c348e2
Ryo ikegata <ryo.ike.6153@gmail.com>
2024-09-20 10:03:33 +0900

    chapter10

--- src/Dollar.php
+++ src/Dollar.php
@@ -1,9 +1,15 @@
 <?php
 
-abstract class Money {
+class Money {
   protected $amount;
   protected $currency;
-  abstract public function times(int $multiplier): Money;
+
+  public function __construct(int $amount, string $currency)
+  {
+    $this->amount = $amount;
+    $this->currency = $currency;
+  }
+  // abstract public function times(int $multiplier): Money;
 
   public function money(int $amount, string $currency)
   {
@@ -19,17 +25,22 @@ abstract class Money {
   public function equals(object $object)
   {
     $money = $object;
-    return $this->amount === $money->amount && get_class($this) === get_class($money);
+    return $this->amount === $money->amount && $this->currency() === $money->currency();
   }
 
   public static function dollar(int $amount): Money
   {
-    return new Dollar($amount, 'USD');
+    return new Money($amount, 'USD');
   }
 
   public static function franc(int $amount): Money
   {
-    return new Franc($amount, 'CHF');
+    return new Money($amount, 'CHF');
+  }
+
+  public function times(int $multiplier): Money
+  {
+    return new Money($this->amount * $multiplier, $this->currency);
   }
 }
 
@@ -42,7 +53,7 @@ class Dollar extends Money
 
   public function times(int $multiplier): Money
   {
-    return Money::dollar($this->amount * $multiplier);
+    return new Money($this->amount * $multiplier, $this->currency);
   }
 }
 class Franc extends Money
@@ -54,6 +65,6 @@ class Franc extends Money
 
   public function times(int $multiplier): Money
   {
-    return Money::franc($this->amount * $multiplier);
+    return new Money($this->amount * $multiplier, $this->currency);
   }
 }
